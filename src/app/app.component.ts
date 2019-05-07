6bfe9514cf92876c746f56ad0dd01e76a90c3488
samrtcode737 <smartcode737@gmail.com>
2019-05-07 11:43:35 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+
+
+
+  constructor(){
+
+  }
+
+  ngAfterViewInit(){
+    this.myCanvas("اهداء");
+  }
+
+  ngOnInit(){
+
+
+  }
+
+  
+  update(value?):string{
+    return value;
+  }
+
+
+  myCanvas(value){
+
+
+    var canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
+    var ctx = canvas.getContext("2d");
+    
+    var img = new Image();
+
+    img.onload = ()=>{
+      ctx.drawImage(img, 0,0, canvas.width, canvas.height);
+      ctx.font = "normal 36px Verdana";
+      ctx.fillText(value, 50,50);
+      document.getElementById("download").setAttribute("href", canvas.toDataURL());
+    }
+
+    // img.src = "https://cdn.cnn.com/cnnnext/dam/assets/190318100224-lionel-messi-exlarge-169.jpg";
+
+    img.src = "assets/cards/9.jpg";
+
+    
+
+
+  }
+
+
+
+
   title = 'canvas';
 }
